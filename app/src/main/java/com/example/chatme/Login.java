a6477e3d792b7f116723dd7ad8f00827f04a4f11
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-05-21 12:27:08 +0530

    Adding

---
+++ app/src/main/java/com/example/chatme/Login.java
@@ -0,0 +1,18 @@
+package com.example.chatme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+}
+}
